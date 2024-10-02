612274ba72d839ff75e4a6911ee49a0cec897b8f
karagozemin <eminkaragoz07@gmail.com>
2024-10-02 19:10:22 +0300

    Numvers

--- 12-numbers/index.js
+++ 12-numbers/index.js
@@ -1,3 +1,23 @@
-const username = ""
-
-console.log("hello");
+const username = "kaptaneth";
+const yas = 25;
+let result;
+result = 25;
+result = Number("25");
+result = parseInt("25");
+result = parseFloat("25.7");
+result = parseInt("27a")
+result = isNaN("a25");
+let myNumber = 10.3456757;
+result = myNumber.toPrecision(6)
+result = myNumber.toFixed(1);
+//! Math Round = içine yazdığın küsürlü sayıyı en yakına yuvarlıyo // math.pow = üssü alma // math.abs = mutlak değer //!
+result = Math.round(1.6);
+result = Math.round(2.49);
+result = Math.ceil(1.2);
+result = Math.floor(2.5);
+result = Math.pow(5, 3);
+result = Math.sqrt(25);
+result = Math.abs(-25);
+result = Math.max(1, 2, 3, 4)
+result = Math.min(1, 2, 3, 4)
+console.log(result);
